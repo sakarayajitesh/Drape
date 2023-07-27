283e4c74573953b6e7050bca19098de09a649e88
Ajitesh Sakaray <sakarayajitesh@gmail.com>
2023-07-27 15:09:26 +0530

    fix: Add card to outfit item in todays outfit

--- lib/routes/outfit_route.dart
+++ lib/routes/outfit_route.dart
@@ -11,8 +11,9 @@ class OutfitRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(OutfitController());
     return Scaffold(
+      appBar: AppBar(title: const Text('Outfits'), centerTitle: true),
       body: Padding(
-        padding: const EdgeInsets.only(top: 16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Obx(
           () => ListView.separated(
             itemBuilder: (BuildContext context, int index) {
@@ -20,56 +21,59 @@ class OutfitRoute extends StatelessWidget {
                   .toList()
                   .reversed
                   .toList()[index];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      outfitMap.key,
-                      style: const TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+              return Card(
+                margin: EdgeInsets.zero,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0),
+                        child: Text(
+                          outfitMap.key,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 16.0),
+                      SizedBox(
+                        height: 80.0,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemCount: outfitMap.value.length,
+                            itemBuilder:
+                                (BuildContext context, int index) {
+                              final outfit = outfitMap.value[index];
+                              double paddingLeft = 8.0;
+                              double paddingRight = 0.0;
+                              if (index == 0) {
+                                paddingLeft = 16.0;
+                              } else if (index ==
+                                  outfitMap.value.length - 1) {
+                                paddingRight = 16.0;
+                              }
+                              return Padding(
+                                padding: EdgeInsets.only(
+                                  left: paddingLeft,
+                                  right: paddingRight,
+                                ),
+                                child: ImageTile(image: outfit.image),
+                              );
+                            }),
+                      ),
+                    ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Divider(),
-                  ),
-                  const SizedBox(
-                    height: 8.0,
-                  ),
-                  SizedBox(
-                    height: 80.0,
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: outfitMap.value.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final outfit = outfitMap.value[index];
-                          double paddingLeft = 8.0;
-                          double paddingRight = 0.0;
-                          if (index == 0) {
-                            paddingLeft = 16.0;
-                          } else if (index == outfitMap.value.length - 1) {
-                            paddingRight = 16.0;
-                          }
-                          return Padding(
-                            padding: EdgeInsets.only(
-                              left: paddingLeft,
-                              right: paddingRight,
-                            ),
-                            child: ImageTile(image: outfit.image),
-                          );
-                        }),
-                  ),
-                ],
+                ),
               );
             },
             separatorBuilder: (BuildContext context, int index) {
               return const SizedBox(
-                height: 16.0,
+                height: 8.0,
               );
             },
             itemCount: controller.outfitMap.entries.length,
